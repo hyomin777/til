352e823747e2e81697a3b0ae275b4b724a0037aa
Hyun-hyo-min <hyomin2677@gmail.com>
2025-02-01 20:18:07 +0900

    빅데이터 플랫폼

--- not_available.R
+++ not_available.R
@@ -6,24 +6,24 @@ copy_iris <- iris
 copy_iris[sample(1:150, 30), 1] <- NA
 
 # 결측치 시각화화
-install.packages('Amelia')
+install.packages("Amelia")
 library(Amelia)
 missmap(copy_iris)
 
 print(dim(copy_iris))
 # 단순 대치법
-copy_iris <- copy_iris[complete.cases(copy_iris),]
+copy_iris <- copy_iris[complete.cases(copy_iris), ]
 print(dim(copy_iris))
 
 copy_iris <- iris
 copy_iris[sample(1:150, 30), 1] <- NA
 
 # 평균 대치법
-meanValue <- mean(copy_iris$Sepal.Length, na.rm=T) # 결측값을 제외한 평균 계산
+meanValue <- mean(copy_iris$Sepal.Length, na.rm = T) # 결측값을 제외한 평균 계산
 copy_iris$Sepal.Length[is.na(copy_iris$Sepal.Length)] <- meanValue # 평균 대치
 
 # 중앙값 대치
-install.packages('DMwR2')
+install.packages("DMwR2")
 library(DMwR2)
 copy_iris <- iris
 copy_iris[sample(1:150, 30), 1] <- NA
@@ -38,7 +38,7 @@ copy_iris <- knnImputation(copy_iris, k = 10)
 copy_iris <- iris
 copy_iris[sample(1:150, 30), 1] <- NA
 library(Amelia)
-iris_imp <- amelia(copy_iris, m=3, cs="Species")
+iris_imp <- amelia(copy_iris, m = 3, cs = "Species")
 # cs는 cross-sectional로 분석에 포함될 정보를 의미
 # 위 amelia에서 m 값을 그대로 imputation의 데이터셋에 사용한다.
 copy_iris$Sepal.Length <- iris_imp$imputations[[3]]$Sepal.Length
--- r_packages.R
+++ r_packages.R
@@ -3,62 +3,62 @@ data <- data.frame(
     semester = c(1, 1, 2, 2),
     math_score = c(60, 90, 70, 90),
     eng_score = c(80, 70, 40, 60)
-    )
+)
 
-install.packages('reshape')
+install.packages("reshape")
 library(reshape)
-melted_data <- melt(data, id=c("student_number", "semester"))
+melted_data <- melt(data, id = c("student_number", "semester"))
 
 casted_data1 <- cast(melted_data, student_number ~ variable, mean)
 casted_data2 <- cast(melted_data, student_number ~ semester, mean)
 casted_data3 <- cast(melted_data, student_number ~ variable, max)
 
-install.packages('sqldf')
+install.packages("sqldf")
 library(sqldf)
-sql_df1 <- sqldf('select * from data')
-sql_df2 <- sqldf('select * from data where student_number = 1')
-sql_df3 <- sqldf('select math_score, eng_score from data group by student_number')
-sql_df4 <- sqldf('select avg(math_score), avg(eng_score) from data group by student_number')
+sql_df1 <- sqldf("select * from data")
+sql_df2 <- sqldf("select * from data where student_number = 1")
+sql_df3 <- sqldf("select math_score, eng_score from data group by student_number")
+sql_df4 <- sqldf("select avg(math_score), avg(eng_score) from data group by student_number")
 
-install.packages('plyr')
+install.packages("plyr")
 library(plyr)
 
-data <- data.frame(class=c('A', 'A', 'B', 'B'), math=c(50, 70, 60, 90), english=c(70, 80, 60, 80))
+data <- data.frame(class = c("A", "A", "B", "B"), math = c(50, 70, 60, 90), english = c(70, 80, 60, 80))
 
-ddply_df1 <- ddply(data, 'class', summarise, math_avg=mean(math), eng_avg=mean(english))
-ddply_df2 <- ddply(data, 'class', transform, math_avg=mean(math), eng_avg=mean(english))
+ddply_df1 <- ddply(data, "class", summarise, math_avg = mean(math), eng_avg = mean(english))
+ddply_df2 <- ddply(data, "class", transform, math_avg = mean(math), eng_avg = mean(english))
 
 data <- data.frame(
-    year=c(2012, 2012, 2012, 2012, 2013, 2013, 2013, 2013),
-    month=c(1, 1, 2, 2, 1, 1, 2, 2),
-    value=c(3, 5, 7, 9, 1, 5, 4, 6)
-    )
+    year = c(2012, 2012, 2012, 2012, 2013, 2013, 2013, 2013),
+    month = c(1, 1, 2, 2, 1, 1, 2, 2),
+    value = c(3, 5, 7, 9, 1, 5, 4, 6)
+)
 
-ddply_df3 <- ddply(data, c('year', 'month'), summarise, value_avg=mean(value))
+ddply_df3 <- ddply(data, c("year", "month"), summarise, value_avg = mean(value))
 ddply_df4 <- ddply(
     data,
-    c('year', 'month'),
+    c("year", "month"),
     function(x) {
-        value_avg <- mean(x$value)  # 평균 계산
-        value_sd <- sd(x$value)    # 표준편차 계산
-        data.frame(avg=value_avg, sd=value_sd, avg_sd = value_avg / value_sd)  # 데이터 프레임으로 반환
+        value_avg <- mean(x$value) # 평균 계산
+        value_sd <- sd(x$value) # 표준편차 계산
+        data.frame(avg = value_avg, sd = value_sd, avg_sd = value_avg / value_sd) # 데이터 프레임으로 반환
     }
 )
 
 
-install.packages('data.table')
+install.packages("data.table")
 library(data.table)
 # 같은 데이터로 4800만 개의 행을 갖는 데이터 프레임과 데이터 테이블을 생성
-year <- rep(c(2012:2015), each=12000000)
-month <- rep(rep(c(1:12), each=1000000), 4)
+year <- rep(c(2012:2015), each = 12000000)
+month <- rep(rep(c(1:12), each = 1000000), 4)
 value <- runif(48000000)
 
 DataFrame <- data.frame(year, month, value)
 DataTable <- as.data.table(DataFrame)
 
 # 검색 시간을 측정
-frame_time <- system.time(DataFrame[DataFrame$year == 2012,])
-table_time <- system.time(DataTable[DataTable$year == 2012,])
+frame_time <- system.time(DataFrame[DataFrame$year == 2012, ])
+table_time <- system.time(DataTable[DataTable$year == 2012, ])
 
 # 데이터 테이블의 year 컬럼에 키 값을 설정
 # 컬럼이 키 값으로 설정될 경우 자동 오름차순 정렬
