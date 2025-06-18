0ea6e9ce863f601221cf63ea4c26933b07b25b9d
hyomin777 <hyomin2677@gmail.com>
2025-06-18 19:54:10 +0900

    learning curves

---
+++ deeplearning/huggingface/llm_course/load_remote_datset.py
@@ -0,0 +1,8 @@
+from datasets import load_dataset
+
+url = "https://github.com/crux82/squad-it/raw/master/"
+data_files = {
+    "train": url + "SQuAD_it-train.json.gz",
+    "test": url + "SQuAD_it-test.json.gz",
+}
+squad_it_dataset = load_dataset("json", data_files=data_files, field="data")
